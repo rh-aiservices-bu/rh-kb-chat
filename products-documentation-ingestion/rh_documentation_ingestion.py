6a5558381c6142f122850bb0f095d3aa3b7b0bde
Guillaume Moutier <guillaume.moutier@gmail.com>
2024-10-04 14:21:22 -0400

    new ingestion

--- products-documentation-ingestion/rh_documentation_ingestion.py
+++ products-documentation-ingestion/rh_documentation_ingestion.py
@@ -1,6 +1,6 @@
 import os
-from langchain.embeddings.huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import Milvus
+from langchain_openai import OpenAIEmbeddings
+from langchain_milvus import Milvus
 import rh_documentation_processing as rhdp
 
 
@@ -8,13 +8,16 @@ os.environ["TRANSFORMERS_VERBOSITY"] = "error"
 os.environ["TRANSFORMERS_NO_ADVISORY_WARNINGS"] = "1"
 
 
-def ingest_documentation(product_info, milvus):
+def ingest_documentation(product_info, milvus_endpoint, embeddings_endpoint):
     """Ingest documentation into Milvus"""
 
-    MILVUS_HOST = milvus["MILVUS_HOST"]
-    MILVUS_PORT = milvus["MILVUS_PORT"]
-    MILVUS_USERNAME = milvus["MILVUS_USERNAME"]
-    MILVUS_PASSWORD = milvus["MILVUS_PASSWORD"]
+    EMBEDDINGS_API_URL = embeddings_endpoint["EMBEDDINGS_API_URL"]
+    EMBEDDINGS_API_KEY = embeddings_endpoint["EMBEDDINGS_API_KEY"]
+
+    MILVUS_HOST = milvus_endpoint["MILVUS_HOST"]
+    MILVUS_PORT = milvus_endpoint["MILVUS_PORT"]
+    MILVUS_USERNAME = milvus_endpoint["MILVUS_USERNAME"]
+    MILVUS_PASSWORD = milvus_endpoint["MILVUS_PASSWORD"]
     MILVUS_COLLECTION = f"{product_info.product}_{product_info.language}_{product_info.version}".replace(
         "-", "_"
     ).replace(
@@ -32,13 +35,15 @@ def ingest_documentation(product_info, milvus):
     # Here we use Nomic AI's Nomic Embed Text model to generate embeddings
     # Adapt to your liking
     model_kwargs = {"trust_remote_code": True, "device": "cuda"}
-    embeddings = HuggingFaceEmbeddings(
-        model_name="nomic-ai/nomic-embed-text-v1",
-        model_kwargs=model_kwargs,
-        show_progress=True,
+    embeddings = OpenAIEmbeddings(
+        openai_api_base=EMBEDDINGS_API_URL,
+        openai_api_key=EMBEDDINGS_API_KEY,
+        model="nomic-embed-text-v1.5",
+        chunk_size="16",
+        show_progress_bar=True
     )
 
-    db = Milvus(
+    vector_store = Milvus(
         embedding_function=embeddings,
         connection_args={
             "host": MILVUS_HOST,
@@ -47,14 +52,14 @@ def ingest_documentation(product_info, milvus):
             "password": MILVUS_PASSWORD,
         },
         collection_name=MILVUS_COLLECTION,
-        metadata_field="metadata",
+        enable_dynamic_field=True,
         text_field="page_content",
         auto_id=True,
         drop_old=True,
     )
 
-    print(f"Uploading documents to collection {MILVUS_COLLECTION}")
+    print(f"Calculating embeddings and uploading documents to collection {MILVUS_COLLECTION}")
 
-    db.add_documents(splits)
+    vector_store.add_documents(splits, batch_size=32)
 
     print("Finished!")
