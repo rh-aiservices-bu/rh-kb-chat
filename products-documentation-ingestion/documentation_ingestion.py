60f32f63f7821d3f89bb37b3a09e20ef1100d4fd
Cansu Kavili <cansukavili@gmail.com>
2025-02-26 16:18:50 +0100

    🫠added traceback🫠

--- products-documentation-ingestion/documentation_ingestion.py
+++ products-documentation-ingestion/documentation_ingestion.py
@@ -13,6 +13,7 @@ def doc_ingest():
     import os
 
     from dotenv import load_dotenv
+    import traceback
 
     import collections_loader as cl
     import milvus_handler
@@ -106,6 +107,7 @@ def doc_ingest():
                     except Exception as e:
                         print(f'❌ Error processing "{collection.collection_full_name}" at version {version.version_number}')
                         print(f'❌ {e}')
+                        traceback.print_exc()
                 else:
                     print("\n⏭️ Collection already present, skipping")
             elif (version.store_directive == 'update'):
@@ -117,6 +119,7 @@ def doc_ingest():
                 except Exception as e:
                     print(f'❌ Error processing "{collection.collection_full_name}" at version {version.version_number}')
                     print(f'❌ {e}')
+                    traceback.print_exc()
             elif (version.store_directive == 'delete'):
                 if (milvus_handler.collection_check(collection_name) is None):
                     print("\n⏭️ No collection present already, skipping")
